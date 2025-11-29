347e419b9582fb8c4ace0f422fda2e682da503f5
Olesia Vysotina <vysotina2002@gmail.com>
2025-11-29 17:07:19 +0100

    feat: Update lesson-7 to lesson-8-9

--- terraform-hw-8-9/backend.tf
+++ terraform-hw-8-9/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-001001-us-east-1" # Назва S3-бакета
-    key            = "lesson-7/terraform.tfstate"   # Шлях до файлу стейту
+    key            = "lesson-8-9/terraform.tfstate"   # Шлях до файлу стейту
     region         = "us-east-1"                    # Регіон AWS
     dynamodb_table = "terraform-locks"
     encrypt        = true
--- terraform-hw-8-9/main.tf
+++ terraform-hw-8-9/main.tf
@@ -12,13 +12,13 @@ module "vpc" {
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  vpc_name           = "lesson-7-vpc"
+  vpc_name           = "lesson-8-9-vpc"
 }
 
 # Підключаємо модуль ECR
 module "ecr" {
   source      = "./modules/ecr"
-  ecr_name    = "lesson-7-ecr"
+  ecr_name    = "lesson-8-9-ecr"
   scan_on_push = true
 }
 
