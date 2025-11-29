79b64ac6c36e4494883bd14841e162f04dac6370
Olesia Vysotina <vysotina2002@gmail.com>
2025-11-29 16:50:45 +0100

    initial commit

---
+++ terraform-hw-8-9/modules/ecr/outputs.tf
@@ -0,0 +1,14 @@
+output "repository_url" {
+  description = "URL створеного репозиторію ECR"
+  value       = aws_ecr_repository.this.repository_url
+}
+
+output "repository_arn" {
+  description = "ARN створеного репозиторію ECR"
+  value       = aws_ecr_repository.this.arn
+}
+
+output "repository_name" {
+  description = "Ім'я створеного репозиторію ECR"
+  value = aws_ecr_repository.this.name
+}
